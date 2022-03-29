53651cd262d53571a5ea120d692fb3ad961fd11d
Cesar Frick <cesarfrick@gmail.com>
2022-03-28 21:27:25 -0500

    Rename Card to MovieCard

---
+++ lib/components/movie_card.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class MovieCard extends StatelessWidget {
+  final String title;
+  final String releaseDate;
+  final String imageSrc;
+  final int rating;
+  final double _containerWidth = 150;
+
+  const MovieCard({
+    Key? key,
+    required this.title,
+    required this.releaseDate,
+    required this.imageSrc,
+    required this.rating,
+  }) : super(key: key);
+
+  double calculateWidth(int percentage) {
+    return _containerWidth * percentage / 100;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: _containerWidth,
+              child: ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  topRight: Radius.circular(10),
+                ),
+                child: Image.asset(imageSrc),
+              ),
+            ),
+            Container(
+              width: calculateWidth(rating),
+              height: 20,
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle, //BoxShape.circle,
+                color: Colors.blue[600],
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    '$rating%',
+                    style: const TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Text(title),
+            Text(releaseDate),
+          ],
+        ),
+      ),
+    );
+  }
+}
