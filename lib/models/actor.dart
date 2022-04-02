4d68ae94d87f59a1acc7d88ba15f1bc7bea775b5
Cesar Frick <cesarfrick@gmail.com>
2022-04-02 15:26:50 -0500

    Create Actor Model

---
+++ lib/models/actor.dart
@@ -0,0 +1,11 @@
+class Actor {
+  final String name;
+  final String imgSrc;
+  final String character;
+
+  const Actor({
+    required this.imgSrc,
+    required this.name,
+    required this.character,
+  });
+}
